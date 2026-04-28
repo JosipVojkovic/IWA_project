ade20ed2864bc16af5eee0b0865f1bd7a26da22f
Josip Vojković <josip.vojkovic@agilo.com>
2026-04-28 16:47:25 +0200

    Add validation error for login and register

--- movies/forms.py
+++ movies/forms.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
+    def clean_email(self):
+        email = self.cleaned_data["email"].strip()
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("A user with that email already exists.")
+        return email
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
