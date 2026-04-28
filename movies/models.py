804d10052d7ab954cc18cf43087fefb45f0403ba
Josip Vojković <josip.vojkovic@agilo.com>
2026-04-28 15:26:38 +0200

    Add Movie, Genre, Director, Actor models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes", null=True, blank=True)
+
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Actor, blank=True)
+    def __str__(self):
+        return self.title
